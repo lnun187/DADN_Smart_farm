d6e55f519fd69ec213872a46b9d1d613abbef8c8
THƯƠNG HOÀI <khanh.tranquoc@hcmut.edu.vn>
2025-03-31 23:18:22 +0700

    new code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // Thông tin Adafruit IO
 const ADAFRUIT_USERNAME = 'lnun187';
-const ADAFRUIT_KEY = 'thay key vào đây';
+const ADAFRUIT_KEY = 'aio_thrZ093ZRs4j8KQgtuUpmOq9pnTD';
 // const ADAFRUIT_USERNAME = 'ivanru';
 // const ADAFRUIT_KEY = 'aio_PZkk338TJbPRk19F41EFxu84pfCT';
 const FEED_NAMES = ['DADN_DHT20_HUM', 'DADN_DHT20_TEMP', 
