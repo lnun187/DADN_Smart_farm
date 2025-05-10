7a0fabf6d529e6750166ac166ff276f39f8fee3c
hoang1603 <nguyenhamlong1@gmail.com>
2025-05-10 21:42:22 +0700

    Image update

--- dashboard/tailwind.config.cjs
+++ dashboard/tailwind.config.cjs
@@ -4,7 +4,19 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeInUp: 'fadeInUp 0.6s ease-out forwards', 
+
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(30px)' }, 
+          '100%': { opacity: '1', transform: 'translateY(0)' }, 
+        },
+      }
+
+    },
   },
   plugins: [],
 });
